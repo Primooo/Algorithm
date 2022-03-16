16efadd2c762bdb751373769b1f9c2f219504609
jinwon <prime4865@gmail.com>
2022-03-17 01:16:48 +0900

    20200316

--- TwoPointer/BOJ_1806.java
+++ TwoPointer/BOJ_1806.java
@@ -3,37 +3,43 @@ package TwoPointer;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-public class BOJ_2003 {
+
+public class BOJ_1806 {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static int[] nums;
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int min = 987654321;
 		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		nums = new int[N];
-		int left = 0, right = 0;
+		int S = Integer.parseInt(st.nextToken());
+		int[] nums = new int[N];
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i=0; i<N; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		int left = 0;
+		int right = 0;
 		int sum = 0;
-		int count = 0;
 		while(true) {
-			if(sum >= M) {
+			if(sum >= S) {
+				if(right-left < min) {
+					min = right-left;
+				}
 				sum -= nums[left++];
 			}
 			else if(right == N) break;
-			else if(sum < M){
+			else if(sum < S) {
 				sum += nums[right++];
 			}
-			
-			if(sum == M) {
-				count++;
-			}
 		}
-		System.out.println(count);
+
+		if(min == 987654321) {
+			System.out.println(0);
+		}
+		else {
+			System.out.println(min);
+		}
 	}
 
 }
