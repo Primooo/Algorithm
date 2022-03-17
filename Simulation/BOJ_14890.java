5f2d1a780d166ddd53ee704c8f6562e092aefe0f
jinwon <prime4865@gmail.com>
2022-03-17 22:05:44 +0900

    20200317

--- Simulation/BOJ_14890.java
+++ Simulation/BOJ_14890.java
@@ -13,6 +13,7 @@ public class BOJ_14890 {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for(int j=0; j<N; j++) {
@@ -20,6 +21,92 @@ public class BOJ_14890 {
 			}
 		}
 		
+		int answer = 0;
+		for(int i=0; i<N; i++) {
+			answer += solution(true, i, N, L);
+			answer += solution(false, i, N, L);
+		}
+		System.out.println(answer);
+	}
+	
+	private static int solution(boolean isRow, int num, int N, int L) {
+		int idx = 1;
+		int[] cp_arr = new int[N];
+		boolean[] visited = new boolean[N];
+		if(isRow) {
+			cp_arr = map[num].clone();
+		}
+		else {
+			for(int i=0; i<N; i++) {
+				cp_arr[i] = map[i][num];
+			}
+		}		
+		
+		int beforeLevel = cp_arr[0];
+		int sameLevelCounter = 1;
+		boolean decendingFlag = false;
+		boolean flag = true;
+		while(idx < N) {
+			int currentLevel = cp_arr[idx];
+			if(Math.abs(beforeLevel - currentLevel) > 1) {
+				flag = false;
+				break;
+			}
+			
+			if(beforeLevel == currentLevel) {
+				sameLevelCounter++;
+				if(sameLevelCounter == L && decendingFlag == true) {
+					decendingFlag = false;
+					sameLevelCounter = 0;
+				}
+				else if(sameLevelCounter != L && decendingFlag == true) {
+					visited[idx] = true;
+				}
+			}
+			else if(beforeLevel < currentLevel) {
+				if(sameLevelCounter < L) {
+					flag = false;
+					break;
+				}				
+				boolean checkVisited = false;
+				for(int i = idx-L; i<idx; i++) {
+					if(visited[i] == true) {
+						checkVisited = true;
+						break;
+					}
+				}
+				if(checkVisited == true) {
+					flag = false;
+					break;
+				}
+				sameLevelCounter = 1;
+			}
+			else {
+				if(decendingFlag == true && sameLevelCounter < L) {
+					flag = false;
+					break;
+				}
+				
+				if(L == 1) {
+					decendingFlag = false;
+				}
+				else {
+					decendingFlag = true;
+				}
+				visited[idx] = true;
+				sameLevelCounter = 1;
+			}
+			
+			if(idx == N-1 && decendingFlag) flag = false;
+			beforeLevel = currentLevel;
+			idx++;
+		}
+		
+		if(flag) {
+			return 1;
+		} else {
+			return 0;
+		}
 	}
 
 }
