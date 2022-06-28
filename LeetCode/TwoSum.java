59be1e6a7201809142956874daa23871de695fe5
prime4865 <prime4865@gmail.com>
2022-06-28 16:09:51 +0900

    20220628

---
+++ LeetCode/TwoSum.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+import java.util.*;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++) {
+            int required = target - nums[i];
+            if(map.containsKey(required)) {
+                int[] answer = {map.get(required), i};
+                return answer;
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
